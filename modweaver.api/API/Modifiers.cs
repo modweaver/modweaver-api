97297a3848fe6e06b6a7b3ed9064f842c4d37936
Ecorous <ecorous@outlook.com>
2024-01-30 16:36:51 +0000

    Template; copy existing APIs

---
+++ modweaver.api/API/Modifiers.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+
+namespace modweaver.api.API {
+    public class WaveModifiers {
+        public static Modifier Create(string  Name,           string Id, bool versus, bool survival, int MaxLevel,
+                                      Sprite? Icon = default, string Description = "") {
+            var modifierData = ScriptableObject.CreateInstance<ModifierData>();
+
+            modifierData.title       = Name;
+            modifierData.key         = Id;
+            modifierData.maxLevel    = MaxLevel;
+            modifierData.icon        = Icon;
+            modifierData.description = Description;
+
+            modifierData.survival = survival;
+            modifierData.versus   = versus;
+
+            return new Modifier(modifierData);
+        }
+
+        public static bool Add(Modifier modifier) {
+            if (ModifierManagerGetNonMaxedSurvivalModsPatch.Mods.Contains(modifier)) return false;
+            ModifierManagerGetNonMaxedSurvivalModsPatch.Mods.Add(modifier);
+            return true;
+
+        }
+
+        public static bool Remove(Modifier modifier) {
+            if (!ModifierManagerGetNonMaxedSurvivalModsPatch.Mods.Contains(modifier)) return false;
+            var mod = ModifierManagerGetNonMaxedSurvivalModsPatch.Mods
+                                                                 .FirstOrDefault(m => m.data.key == modifier.data.key);
+            return ModifierManagerGetNonMaxedSurvivalModsPatch.Mods.Remove(mod);
+
+        }
+
+        public static Modifier Get(string Id) {
+            return ModifierManagerGetNonMaxedSurvivalModsPatch.Mods.First(m => m != null && m.data.key == Id);
+        }
+
+        public static void Give(string id, int level, bool survival, bool versus) {
+            var mod = Get(id);
+            if (survival)
+                mod.levelInSurvival = level;
+            if (versus)
+                mod.levelInVersus = level;
+        }
+
+        [HarmonyPatch(typeof(ModifierManager), "GetNonMaxedSurvivalMods")]
+        internal class ModifierManagerGetNonMaxedSurvivalModsPatch {
+            internal static List<Modifier> Mods { get; private set; } = new();
+
+            internal static bool Prefix(ModifierManager __instance, ref List<Modifier> __result) {
+                var templist = (from m in Traverse.Create(__instance).Field<List<Modifier>>("_modifiers").Value
+                                where m.levelInSurvival < m.data.maxLevel && m.data.survival
+                                select m).ToList();
+                if (Mods.Count > 0) {
+                    foreach (Modifier mod in Mods) {
+                        if (mod == null || templist.Contains(mod))
+                            continue;
+
+                        templist.Add(mod);
+                    }
+                }
+
+                __result = templist;
+                return false;
+            }
+
+            internal static void Postfix(ref List<Modifier> __result) {
+                Mods = __result;
+            }
+        }
+    }
+}
---
+++ modweaver.api/API/Weapons.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+
+namespace modweaver.api.API {
+    public class Weapons {
+        [HarmonyPatch(typeof(WeaponManager))]
+        public class WeaponManagerPatch {
+            public static Weapon? EquippedWeapon { get; internal set; }
+
+            [HarmonyPatch("EquipWeapon")]
+            [HarmonyPostfix]
+            internal static void EquipWeaponPostfix(ref Weapon weapon) {
+                EquippedWeapon = weapon;
+                if (EquippedWeapon != null) {
+                    weapon = EquippedWeapon;
+                }
+            }
+
+            [HarmonyPatch("EquipWeapon")]
+            [HarmonyPrefix]
+            internal static void EquipWeaponPrefix(ref Weapon weapon) {
+                if (EquippedWeapon != null) {
+                    weapon = EquippedWeapon;
+                }
+            }
+
+            [HarmonyPatch("UnEquipWeapon")]
+            [HarmonyPostfix]
+            internal static void UnEquipWeaponPostfix() {
+                EquippedWeapon = null;
+            }
+        }
+
+        public static Weapon? GetEquippedWeapon() {
+            return WeaponManagerPatch.EquippedWeapon;
+        }
+
+        // * To be done
+        public static void SetEquippedWeapon(Weapon weapon) {
+            WeaponManagerPatch.EquippedWeapon = weapon;
+        }
+
+        // Add weapon
+        public static Weapon CreateWeapon(int ammo, List<Weapon.WeaponType> weaponType, string label) {
+            Weapon weapon = new() {
+                maxAmmo = ammo,
+                ammo    = ammo,
+                label   = label,
+                type    = weaponType
+            };
+
+            return weapon;
+        }
+
+
+        //* To be done
+
+        private List<SpawnableWeapon> getVersusWeapons() {
+            bool                  flag = GameSettings.Instance == null;
+            List<SpawnableWeapon> result;
+            if (flag) {
+                result = null;
+            }
+            else {
+                List<SpawnableWeapon> list = GameSettings.Instance.AvailableVersusWeapons();
+                result = list;
+            }
+
+            return result;
+        }
+
+        /*private void spawnWeapon(int selectedWeapon) {
+            GameObject gameObject = GameObject.FindGameObjectWithTag("PlayerRigidbody");
+            Vector2    a          = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            //bool flag = !Physics2D.Raycast(a + new Vector2(0f, 10f), gameObject.transform.up, 0.1f,
+            //                               GameController.instance.worldLayers);
+            bool flag2 = flag;
+            if (flag2) {
+                //Object.Instantiate(getVersusWeapons()[selectedWeapon].weapon, gameObject.transform.position, gameObject.transform.rotation);
+                ObjectSpawner.Instantiate(getVersusWeapons()[selectedWeapon].weaponObject,
+                                          gameObject.transform.position,
+                                          gameObject.transform.rotation);
+            }
+        }*/
+    }
+}
