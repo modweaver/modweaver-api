f9bf95dc2ac9c4da745708cb5416a98122f38e05
AnriiAndrushko <andry.i.andrushko@gmail.com>
2024-02-01 23:08:49 +0200

    Added custom weapons api

--- modweaver.testmod/ModMain.cs
+++ modweaver.testmod/ModMain.cs
@@ -1,52 +1,86 @@
-using System;
-using System.IO;
 using HarmonyLib;
+using modweaver.api.API;
 using modweaver.core;
+using UnityEngine;
 
 
-namespace modweaver.testmod {
+namespace modweaver.testmod
+{
     [ModMainClass]
     // ReSharper disable once UnusedType.Global (because it is used!!)
-    public class ModMain : Mod {
-        public override void Init() {
+    public class ModMain : Mod
+    {
+        CustomWeapon weapon1;
+        public override void Init()
+        {
             //test logger
             Logger.Trace("Trace!");
             Logger.Debug("Debug!");
             Logger.Info("Info!");
             Logger.Warn("Warn!");
             Logger.Error("Error!");
-            
+
+            //test NewWeaponsManager
+            weapon1 = new CustomWeapon("test weapon", NewWeaponsManager.WeaponType.ParticleBlade);
+            NewWeaponsManager.AddNewWeapon(weapon1);
+            NewWeaponsManager.OnInitCompleted += Weapon1;
+
             Logger.Info("Test mod init method is called.");
-            Harmony harmony = new Harmony(Metadata.id); 
+            Harmony harmony = new Harmony(Metadata.id);
             Logger.Info("Running patches!");
             harmony.PatchAll();
             Logger.Info("Patched!");
         }
 
-        public override void Ready() {
-            Logger.Info("Test mod is ready!");
-            
+        //sample weapon
+        void Weapon1()
+        {
+            weapon1.WeaponObject.GetComponent<ParticleBlade>().baseSize = new Vector2(10, 100);
+
+            //this currently unused since there no UnityEngine.ImageConversionModule.dll in modweaver
+
+            //string path = "modweaver\\mods\\Testmod\\weapon1.png"; //path to your texture
+            //int textureSize = 1000;
+            //SpriteRenderer sr = weapon1.WeaponObject.GetComponent<SpriteRenderer>();
+            //sr.sprite = Sprite.Create(LoadTextureFromFile(path, textureSize, textureSize), new Rect(0, 0, textureSize, textureSize), new Vector2(0.5f, 0.5f));
+        }
+
+        //private Texture2D LoadTextureFromFile(string filePath, int width, int height)
+        //{
+        //    byte[] fileData = File.ReadAllBytes(filePath);
+        //    Texture2D texture = new Texture2D(width, height);
+        //    texture.LoadImage(fileData);
+
+        //    return texture;
+        //}
+
+        public override void Ready()
+        {
+            Logger.Info("Testmod is ready!");
+
             Config.set("test1", 42);
             Config.set("test2", true, "otherfile");
 
             var test1 = Config.get("test1", -1);
             var test2 = Config.get("test2", false, "otherfile");
-            
+
             Logger.Debug("Config test values: {V1}, {V2}", test1, test2);
         }
 
-        public override void OnGUI(ModsMenuPopup ui) {
-            ui.CreateDivider();
-            ui.CreateTitle("I'm a cool mod :3");
-        }
-    }
+        public override void OnGUI(ModsMenuPopup ui)
+        {
 
-    [HarmonyPatch(typeof(VersionNumberTextMesh), "Start")]
-    public static class VNTMPatch {
-        
-        [HarmonyPostfix]
-        public static void Postfix(ref VersionNumberTextMesh __instance) {
-            __instance.textMesh.text += "\nhello world :3";
+        }
+
+        [HarmonyPatch(typeof(VersionNumberTextMesh), "Start")]
+        public static class VNTMPatch
+        {
+
+            [HarmonyPostfix]
+            public static void Postfix(ref VersionNumberTextMesh __instance)
+            {
+                __instance.textMesh.text += "\nhello world :3";
+            }
         }
     }
 }
